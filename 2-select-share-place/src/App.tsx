73182b896c91194ca13593b084fe75b081dc8230
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2022-12-20 15:14:35 +0300

    start 3

--- 2-select-share-place/src/App.tsx
+++ 2-select-share-place/src/App.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
+
 import './App.css';
+import axios from 'axios';
+
+interface Goggle{
+results:{geometry:{location:{lat:number,lng:number}}}[],
+status:"OK" | "ZERO_RESULTS" 
+}
 
 function App() {
-  console.log(process.env.REACT_APP_KEY);
+  const [asd,setAsd]= useState("")
+const textRef = React.useRef<HTMLInputElement | null>();
+
+  // console.log(process.env.REACT_APP_KEY);
+const searchSubmit = (e: React.FormEvent<HTMLFormElement>):void => {
+  e.preventDefault()
+  // let asd:string | ""=textRef.current?.value
+  console.log(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+      asd
+    )}&key=${process.env.REACT_APP_KEY}`
+  );
+};
+
+
+
   return (
     <div className="App">
-     <div className="map">
-      <p>Adresiniz giriniz...</p>
-     </div>
-     <form>
-      <input type="text" id='address' />
-      <button type='submit'>Ara</button>
-     </form>
+      <div className="map">
+        <p>Adresiniz giriniz...</p>
+      </div>
+      <form onSubmit={searchSubmit}>
+        <input onChange={(e)=>setAsd(e.target.value)}  type="text" id="address" />
+        <button type="submit">Ara</button>
+      </form>
     </div>
   );
 }
