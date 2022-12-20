a49bde7815209c0ceb5f9f244507200f5c82e0c0
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2022-12-20 03:30:54 +0300

    düzeltme

--- 1-todo-list/src/App.tsx
+++ 1-todo-list/src/App.tsx
@@ -2,21 +2,20 @@ import React, { useState } from "react";
 import { v4 as uuidV4 } from "uuid";
 import "./App.css";
 
-interface Task{
-  id:string
-  title:string
-  completed:boolean
-  createdAt:Date
+interface Task {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
 }
 
-
 function App() {
-   const textRef = React.useRef<HTMLInputElement | null>(null);
-const [tasks, setTasks] = useState(loadTasks());
+  const textRef = React.useRef<HTMLInputElement | null>(null);
+  const [tasks, setTasks] = useState(loadTasks());
 
   function saveTasks() {
     localStorage.setItem("TASK", JSON.stringify(tasks));
-    loadTasks()
+    loadTasks();
   }
 
   function loadTasks(): Task[] {
@@ -25,46 +24,25 @@ const [tasks, setTasks] = useState(loadTasks());
     return JSON.parse(taskJSON);
   }
 
-const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-  e.preventDefault()
-if(textRef.current?.value=== ""){
-  return alert("Eksik giriş")
-}
-const newTask: Task = {
-  id: uuidV4(),
-  title: (textRef.current as HTMLInputElement).value,
-  completed: false,
-  createdAt: new Date(),
-};
-setTasks([...tasks,newTask]);
-// tasks.push(newTask);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (textRef.current?.value === "") {
+      return alert("Eksik giriş");
+    }
+    const newTask: Task = {
+      id: uuidV4(),
+      title: (textRef.current as HTMLInputElement).value,
+      completed: false,
+      createdAt: new Date(),
+    };
+    setTasks([...tasks, newTask]);
 
-(textRef.current as HTMLInputElement).value = "";
-saveTasks()
-};
-// function addListItem(task: Task) {
-//   const item = document.createElement("li");
-//   const label = document.createElement("label");
-//   const checkbox = document.createElement("input");
-//   checkbox.addEventListener("change", () => {
-//     task.completed = checkbox.checked;
-//     saveTasks();
-//   });
-//   checkbox.type = "checkbox";
-//   checkbox.checked = task.completed;
-//   label.append(checkbox, task.title);
-//   item.append(label);
-//   list?.append(item);
-// }
-const ethem = (task: React.ChangeEvent<HTMLInputElement>): void => {
-  console.log(task);
-  console.log(task.currentTarget.value)
-};
+    (textRef.current as HTMLInputElement).value = "";
+    saveTasks();
+  };
 
-
-const renderNots = (): JSX.Element[] => {
+  const renderNots = (): JSX.Element[] => {
     return tasks?.map((task) => {
-
       return (
         <li key={task.id}>
           <span>{task.title}</span>
@@ -74,24 +52,22 @@ const renderNots = (): JSX.Element[] => {
             onChange={() => {
               task.completed = !task.completed;
               saveTasks();
-              setTasks(tasks.map((tas)=>tas));
-              
+              setTasks(tasks.map((tas) => tas));
             }}
             checked={task.completed}
           />
         </li>
       );
-   
-        })
-    }  
-  
+    });
+  };
+
   return (
     <div className="App">
       <form id="new-task-form" onSubmit={(e) => handleSubmit(e)}>
         <input ref={textRef} type="text" id="new-task-title" />
         <button type="submit">Add</button>
       </form>
-      <ul className="list" >{renderNots()}</ul>
+      <ul className="list">{renderNots()}</ul>
     </div>
   );
 }
