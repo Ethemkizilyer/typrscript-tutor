422e0de5075ccfe24a359ec6b17948b5fc680db4
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2022-12-20 04:27:41 +0300

    start

---
+++ 2-select-share-place/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  console.log(process.env.REACT_APP_KEY);
+  return (
+    <div className="App">
+     <div className="map">
+      <p>Adresiniz giriniz...</p>
+     </div>
+     <form>
+      <input type="text" id='address' />
+      <button type='submit'>Ara</button>
+     </form>
+    </div>
+  );
+}
+
+export default App;
+// AIzaSyBuBKpZwsIiLbcaJ2IGRUSflSfjmOk6mt4;
